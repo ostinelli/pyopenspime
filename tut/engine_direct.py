16705b93bf31356910fd8cf3da487d0acb0343c0
ostinelli <ostinelli@d311e072-dd4e-0410-8d45-0b9927e6b65b>
2008-11-02 10:00:13 +0000

    @tut/engine_direct.py library path bug corrected.

--- tut/engine_direct.py
+++ tut/engine_direct.py
@@ -63,7 +63,7 @@ def add_to_sys_path(paths):
             sys.path.append(path)
 import sys, os
 os.chdir(os.path.abspath(os.path.dirname(sys.argv[0])))
-add_to_sys_path( ('lib',) )
+add_to_sys_path( ('../lib',) )
 import logging, pyopenspime.xmpp.protocol, pyopenspime.protocol.extension.core.datareporting, pyopenspime.protocol.extension.xmpp.pubkey
 from pyopenspime.engine import StanzaInterpreter
 
